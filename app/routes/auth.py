e1071f628d800deabfc907cb5ac320b32924a9c8
Volodymyr Zuyev <volodymyrzuyev@gmail.com>
2025-11-18 20:44:39 -0600

    Reworked auth endpoint to have consistent jwt

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,34 +1,47 @@
 from fastapi import APIRouter
 from fastapi.responses import RedirectResponse
 from dotenv import load_dotenv
-import os
+import databaseStuff.db_controller as db_controller
+import random
+import string
+import jwt
 
 from models import provider
- 
+
 load_dotenv()
 
-router = APIRouter(
-    prefix="/auth",
-    tags=["auth"],
-    responses={404: {"description": "invalid location"}},
-)
 
-
-PROVIDERS = {
-    "google": provider.Google(
-        clientID=os.getenv("GOOGLE_CLIENT_ID"),
-        clientSecret=os.getenv("GOOGLE_CLIENT_SECRET"),
-        redirectURI=os.getenv("GOOGLE_REDIRECT"),
+def createAuthRouter(
+    providers: dict[str, provider.providers],
+    db: db_controller.DatabaseController,
+    secretKey: str,
+) -> APIRouter:
+    router = APIRouter(
+        prefix="/auth",
+        tags=["auth"],
+        responses={404: {"description": "invalid location"}},
     )
-}
 
+    @router.get("/login/{provider}/")
+    async def login(provider):
+        return RedirectResponse(providers[provider].getAuthUrl())
 
-@router.get("/login/{pr}/")
-async def read_items(pr):
-    return RedirectResponse(PROVIDERS[pr].getAuthUrl())
+    @router.get("/callback/{provider}")
+    async def callback(provider: str, code: str):
+        token = providers[provider].callback(code)
+        userID = db.get_user_id_from_token(token, provider)
+        if userID is None:
+            username = "".join(
+                random.choices(string.ascii_letters + string.digits, k=20)
+            )
+            password = "".join(
+                random.choices(string.ascii_letters + string.digits, k=20)
+            )
+            userID = db.create_user(username, password, False)
+            db.add_user_id_to_token(token, provider, userID)
 
+        internalJWT = jwt.encode({"id": userID}, secretKey, algorithm="HS256")
 
-@router.get("/callback/{pr}")
-async def read_item(pr: str, code: str):
-    return {"status": "success", "jwt": PROVIDERS[pr].callback(code)}
+        return {"status": "success", "jwt": internalJWT}
 
+    return router
